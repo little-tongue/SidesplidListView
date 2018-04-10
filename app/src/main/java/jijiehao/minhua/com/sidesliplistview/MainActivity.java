5a3fc0a850f3beede73c1002f23d4fd0c27b3b3a
moodstrikerdd <dingdong@zafh.com.cn>
2018-04-10 18:14:09 +0800

    First

---
+++ app/src/main/java/jijiehao/minhua/com/sidesliplistview/MainActivity.java
@@ -0,0 +1,80 @@
+package jijiehao.minhua.com.sidesliplistview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jijiehao.minhua.com.sidesliplistview.adapter.CommonAdapter;
+import jijiehao.minhua.com.sidesliplistview.adapter.ViewHolder;
+
+/**
+ * @author moodstrikerdd
+ * @date 2018/4/10
+ * @label
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeSwipeRefreshLayout relativeSwipeRefreshLayout;
+    private SideslipListView sideslipListView;
+
+    private CommonAdapter<String> adapter;
+    private List<String> data = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+        for (int i = 0; i < 10; i++) {
+            data.add("item" + (i + 1));
+        }
+        adapter.notifyDataSetChanged();
+        relativeSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    private void initView() {
+        relativeSwipeRefreshLayout = this.findViewById(R.id.refresh);
+        relativeSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initData();
+            }
+        });
+
+        sideslipListView = this.findViewById(R.id.slvContent);
+        adapter = new CommonAdapter<String>(this, R.layout.item_main, data) {
+
+            @Override
+            public void convert(ViewHolder holder, String s) {
+                holder.setText(R.id.tvItemName, s);
+                holder.setOnClickListener(R.id.tvDelete, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "删除", Toast.LENGTH_SHORT).show();
+                        sideslipListView.turnToNormal();
+                    }
+                });
+                holder.setOnClickListener(R.id.tvPay, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "已换", Toast.LENGTH_SHORT).show();
+                        sideslipListView.turnToNormal();
+                    }
+                });
+            }
+        };
+        sideslipListView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/jijiehao/minhua/com/sidesliplistview/RelativeSwipeRefreshLayout.java
@@ -0,0 +1,72 @@
+package jijiehao.minhua.com.sidesliplistview;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+
+/**
+ * @author moodstrikerdd
+ * @date 2018/4/10
+ * @label
+ */
+
+public class RelativeSwipeRefreshLayout extends SwipeRefreshLayout {
+    /**
+     * 是否让子view处理touch事件
+     */
+    private boolean letChildDealTouchEvent;
+
+    public RelativeSwipeRefreshLayout(Context context) {
+        super(context);
+    }
+
+    public RelativeSwipeRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    private float startX;
+    private float startY;
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                // 记录手指按下的位置
+                startY = ev.getY();
+                startX = ev.getX();
+                // 初始化标记
+                letChildDealTouchEvent = false;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // 如果子view正在拖拽中，那么不拦截它的事件，直接return false；
+                if (letChildDealTouchEvent) {
+                    return false;
+                }
+
+                // 获取当前手指位置
+                float endY = ev.getY();
+                float endX = ev.getX();
+                float distanceX = Math.abs(endX - startX);
+                float distanceY = Math.abs(endY - startY);
+                // 如果X轴位移大于Y轴位移，那么将事件交给子View处理
+                if (distanceX > ViewConfiguration.get(getContext()).getScaledTouchSlop() && distanceX > distanceY) {
+                    letChildDealTouchEvent = true;
+                    return false;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                // 初始化标记
+                letChildDealTouchEvent = false;
+                break;
+            default:
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+}
